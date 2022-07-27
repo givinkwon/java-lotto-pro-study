b4c3148da2b661feba2a93f00d1da14fd07d394a
givinkwon <rlqls505@naver.com>
2022-07-27 14:51:07 +0900

    add : Calculator 추가

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+public class Caculator {
+  int add(int i, int j) {
+    return i + j; 
+  }
+  int subtract(int i, int j) {
+    return i - j; 
+  }
+  int multiply(int i, int j) {
+    return i * j; 
+  }
+  int divide(int i, int j) {
+    return i / j; 
+  }
+}
